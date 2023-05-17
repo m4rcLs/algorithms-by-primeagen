05acdca655a8b383da2baac5aa58bc7bd16182a2
Marcel <marcel.siegert@gmx.net>
2023-05-17 18:17:41 +0200

    feat: complete arrays chapter

---
+++ src/arrays.ts
@@ -0,0 +1,13 @@
+//  Single continuous memory space
+// index = type bit size * i
+// e. g. int32[3] => 96 bit; arr[1] starts at bit 33
+// a + width * offset = O(1)
+const a = new ArrayBuffer(6);
+const a8 = new Uint8Array(a);
+a8[0] = 45;
+a8[2] = 45;
+
+const a16 = new Uint16Array(a);
+a16[2] = 0x4545;
+
+console.log({a, a8, a16})
